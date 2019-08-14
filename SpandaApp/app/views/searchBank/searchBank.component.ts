e9a1f3f5335aac729823637c706e68cc219bb491
derzaarsad <derza.arsad@yahoo.com>
2019-08-14 23:51:25 +0200

    Online Banking login Webform in WebView

--- SpandaApp/app/views/searchBank/searchBank.component.ts
+++ SpandaApp/app/views/searchBank/searchBank.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import { RouterExtensions } from "nativescript-angular/router";
 import { NavigationOptions } from "nativescript-angular/router/ns-location-strategy";
 import { Page, EventData } from "tns-core-modules/ui/page";
@@ -9,6 +9,9 @@ import * as appSettings from "tns-core-modules/application-settings";
 import { BankService } from "../../services/bank.service";
 import { Bank } from "~/models/bank.model";
 
+import { WebView, LoadEventData } from "tns-core-modules/ui/web-view";
+import { Label } from "tns-core-modules/ui/label";
+
 @Component({
     selector: "searchBank",
     templateUrl: "./searchBank.html",
@@ -16,9 +19,13 @@ import { Bank } from "~/models/bank.model";
     providers: [BankService]
 })
 export class SearchBankComponent implements OnInit {
-    private SearchedBlz: string;
+    private SearchedBlz: string = "";
     private bank: Bank;
-    private EnableBank: boolean;
+    private EnableBank: boolean = false;
+    private webViewSrc: string = "";
+
+    @ViewChild("myWebView", { read: ElementRef, static: false }) webViewRef: ElementRef;
+    @ViewChild("labelResult", { read: ElementRef, static: false }) labelResultRef: ElementRef;
 
     constructor(
         private routerExtensions: RouterExtensions,
@@ -28,10 +35,24 @@ export class SearchBankComponent implements OnInit {
 
     ngOnInit(): void {
         this.page.actionBarHidden = true;
-        this.SearchedBlz = "";
-        this.bank = new Bank();
-        this.bank.Name = "Unknown Bank";
-        this.EnableBank = false;
+    }
+
+    ngAfterViewInit() {
+        let webview: WebView = this.webViewRef.nativeElement;
+        let label: Label = this.labelResultRef.nativeElement;
+        label.text = "WebView is still loading...";
+
+        webview.on(WebView.loadFinishedEvent, function (args: LoadEventData) {
+            let message;
+            if (!args.error) {
+                message = "WebView finished loading of " + args.url;
+            } else {
+                message = "Error loading " + args.url + ": " + args.error;
+            }
+
+            label.text = message;
+            console.log("WebView message - " + message);
+        });
     }
 
     alert(message: string) {
@@ -58,6 +79,7 @@ export class SearchBankComponent implements OnInit {
 
     private onAddAccount() {
         this.bankService.getWebformIdAndToken(this.bank, appSettings.getString("token_type"), appSettings.getString("access_token")).then((res) => {
+            this.webViewSrc = "https://sandbox.finapi.io/webForm/" + res[1];
             console.log(res[0]);
             console.log(res[1]);
             console.log(res[2]);
