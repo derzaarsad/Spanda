fc6b15491c7c73cd1f96020ff09be5e16a9cf221
Kiril Tonev <kiril.tonev@gmail.com>
2019-10-13 10:48:52 +0200

    Remove useless bin file.

--- SpandaServerless/lambda/bin/initialize-handlers.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const handlers = require('../lib/lambda-handlers')
-const services = handlers.initializeFromEnvironmentObject(process.env)
-
-const bankController = services.bankController
-
-bankController.getBankByBLZ('66070024').then(resp => console.log(resp))
