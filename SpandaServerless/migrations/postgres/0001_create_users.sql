8d3ba91e2178be83818920b68ff79244d689254e
Kiril Tonev <kiril.tonev@gmail.com>
2019-11-10 21:08:34 +0100

    AB#7 Add database migrations.

---
+++ SpandaServerless/migrations/postgres/0001_create_users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users
+(
+    username text COLLATE pg_catalog."default" NOT NULL,
+    creationdate timestamp without time zone NOT NULL,
+    allowance numeric,
+    isallowanceready boolean NOT NULL,
+    email text COLLATE pg_catalog."default",
+    phone text COLLATE pg_catalog."default",
+    isautoupdateenabled boolean NOT NULL,
+    bankconnectionids integer[],
+    activewebformid integer,
+    activewebformauth text COLLATE pg_catalog."default",
+    CONSTRAINT users_pkey PRIMARY KEY (username)
+)
---
+++ SpandaServerless/migrations/postgres/0004_create_transactions_accountid_seq.sql
@@ -0,0 +1,6 @@
+CREATE SEQUENCE transactions_accountId_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 2147483647
+    CACHE 1;
