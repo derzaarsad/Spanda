eba0db662846b30ce799ba592be9bd43606c9e9b
derzaarsad <derza.arsad@yahoo.com>
2019-11-10 02:13:45 +0100

    bugfix byte like instead of str

--- determineAzureJobs.py
+++ determineAzureJobs.py
@@ -1,16 +1,16 @@
 import sys
 from subprocess import check_output
-changedFilesList = check_output([b'git', b'diff', b'--name-only']).split(b'\n')
+changedFilesList = check_output(['git', 'diff', '--name-only']).split(b'\n')
 anyApp = any(item.startswith(b'SpandaApp/') for item in changedFilesList)
 anyServerless = any(item.startswith(b'SpandaServerless/') for item in changedFilesList)
 
-retText = b'##vso[task.setvariable variable=jobName;isOutput=true]'
+retText = '##vso[task.setvariable variable=jobName;isOutput=true]'
 
 if anyApp == False:
-    print(retText + b'Serverless')
+    print(retText + 'Serverless')
     sys.exit(0)
 if anyServerless == False:
-    print(retText + b'App')
+    print(retText + 'App')
     sys.exit(0)
-print(retText + b'All')
+print(retText + 'All')
 sys.exit(0)
