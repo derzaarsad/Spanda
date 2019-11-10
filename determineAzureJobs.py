181792ea828c19c0baecf804c1f53b42eeab586c
derzaarsad <derza.arsad@yahoo.com>
2019-11-10 02:23:47 +0100

    determination compared to origin/master

--- determineAzureJobs.py
+++ determineAzureJobs.py
@@ -1,7 +1,7 @@
 import sys
 from subprocess import check_output
 currentBranch = check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).split(b'\n')
-changedFilesList = check_output(['git', 'diff', '--name-only', currentBranch[0], 'origin/master']).split(b'\n')
+changedFilesList = check_output(['git', 'diff', '--name-only', currentBranch[0].decode('utf-8'), 'origin/master']).split(b'\n')
 anyApp = any(item.startswith(b'SpandaApp/') for item in changedFilesList)
 anyServerless = any(item.startswith(b'SpandaServerless/') for item in changedFilesList)
 
