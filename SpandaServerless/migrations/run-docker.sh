6408c2abf1327a6c625dc1db22382baef1349718
Kiril Tonev <kiril.tonev@gmail.com>
2019-11-13 22:28:56 +0100

    AB#10 Improve docker script.

--- SpandaServerless/migrations/run-docker.sh
+++ SpandaServerless/migrations/run-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-docker run --name postgres -v ${PWD}/postgres:/docker-entrypoint-initdb.d postgres:12
+docker run --rm -p 5432:5432 --name postgres -v ${PWD}/postgres:/docker-entrypoint-initdb.d postgres:12
