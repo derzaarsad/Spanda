d28dd1017642fae148d026c0e39858b2cc89dddd
derzaarsad <derza.arsad@yahoo.com>
2019-11-14 23:43:19 +0100

    reset test

--- SpandaServerless/deleteUser.py
+++ SpandaServerless/deleteUser.py
@@ -3,8 +3,6 @@
 import json
 import sys
 from subprocess import check_output
-print(sys.argv)
-sys.exit(0)
 response = check_output(['curl', '-X', 'POST', '-H','Content-Type:application/x-www-form-urlencoded', '--data', 'grant_type=client_credentials&client_id=' + sys.argv[1] + '&client_secret=' + sys.argv[2], 'https://sandbox.finapi.io/oauth/token']).split(b'\n')[0]
 access_token = json.loads(response)["access_token"]
 response = check_output(['curl', '-X', 'POST', '-H','Content-Type:application/json', '-H','Authorization:Bearer ' + access_token, '--data', '{\"userIds\":[\"' + sys.argv[3] + '\"]}', 'https://sandbox.finapi.io/api/v1/mandatorAdmin/deleteUsers']).split(b'\n')[0]
