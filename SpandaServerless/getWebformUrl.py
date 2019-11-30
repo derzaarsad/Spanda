cba92fe796ec3cf8464d7f013b2c3f08edf7ea0c
derzaarsad <derza.arsad@yahoo.com>
2019-11-30 22:56:39 +0100

    finAPI user added for getWebform test AB#5

---
+++ SpandaServerless/getWebformUrl.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import json
+import sys
+from subprocess import check_output
+response = check_output(['curl', '-X', 'POST', '-H','Content-Type:application/json', '-H','Authorization:Bearer ' + sys.argv[1], '--data', '{\"bankId\":277672}', 'https://sandbox.finapi.io/api/v1/bankConnections/import']).split(b'\n')[0]
+print(response)
+sys.exit(0)
