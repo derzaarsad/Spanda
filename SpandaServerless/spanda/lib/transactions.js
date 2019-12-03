ee734fdce26fb303d368adf092de2e592ab7ae10
derzaarsad <derza.arsad@yahoo.com>
2019-12-03 23:28:07 +0100

    transactions.js and unit test-postgres-transactions-repo added AB#71

---
+++ SpandaServerless/spanda/lib/transactions.js
@@ -0,0 +1,97 @@
+'use strict'
+
+const createTransaction = (
+    id, accountId, amount, bookingDate, purpose,
+    counterPartName, counterPartAccountNumber, counterPartIban, counterPartBlz, counterPartBic, counterPartBankName
+    ) => {
+  return {
+    'id': id,
+    'accountId': accountId,
+    'amount': amount,
+    'bookingDate': bookingDate,
+    'purpose': purpose,
+    'counterPartName': counterPartName,
+    'counterPartAccountNumber': counterPartAccountNumber,
+    'counterPartIban': counterPartIban,
+    'counterPartBlz': counterPartBlz,
+    'counterPartBic': counterPartBic,
+    'counterPartBankName': counterPartBankName
+  }
+}
+
+exports.NewInMemoryRepository = () => {
+  const repository = {}
+
+  return {
+    new: createTransaction,
+
+    findById: async (id) => {
+      return repository[id]
+    },
+
+    save: async (transaction) => {
+      repository[transaction.id] = transaction
+      return transaction
+    },
+
+    deleteAll: async () => {
+      Object.keys(repository).forEach(function(key) { delete repository[key] })
+    }
+  }
+}
+
+exports.NewPostgreSQLRepository = (pool, format, schema, types) => {
+  const findByIdQuery = (id) => {
+    return format('SELECT * FROM %I WHERE id = %L LIMIT 1',
+      schema.tableName, id.toString());
+  }
+
+  const deleteAllQuery = format('DELETE FROM %I', schema.tableName);
+
+  const saveQuery = (transaction) => {
+    const tableName = schema.tableName;
+    const row = schema.asRow(transaction);
+    const attributes = schema.attributes;
+    const columns = schema.columns;
+
+    return format('INSERT INTO %I (%s) VALUES (%L) ON CONFLICT (%I) DO UPDATE SET (%s) = (%L) WHERE %I.%I = %L',
+      tableName, attributes, row, columns.id, attributes, row, tableName, columns.id, transaction.id);
+  }
+
+  return {
+    new: createTransaction,
+
+    findById: async (id) => {
+      const client = await pool.connect();
+      const params = {
+        text: findByIdQuery(id),
+        rowMode: 'array',
+        types: types
+      }
+
+      return client.query(params)
+        .then(res => (res.rowCount > 0) ? schema.asObject(res.rows[0]) : null)
+        .finally(() => { client.release() })
+    },
+
+    save: async (transaction) => {
+      const client = await pool.connect();
+
+      return client.query(saveQuery(transaction))
+        .then(() => transaction)
+        .finally(() => { client.release() });
+    },
+
+    deleteAll: async () => {
+      const client = await pool.connect();
+
+      return client.query(deleteAllQuery)
+        .then(res => res)
+        .finally(() => { client.release() });
+    },
+
+    findByIdQuery: findByIdQuery,
+    saveQuery: saveQuery,
+    deleteAllQuery: deleteAllQuery
+  }
+}
---
+++ SpandaServerless/spanda/tests/unit/test-postgres-transactions-repo.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/* eslint-env node, mocha */
+const chai = require('chai');
+const expect = chai.expect;
+
+const format = require('pg-format');
+
+const Transactions = require('../../lib/transactions');
+const transactionsSchema = require('../../lib/schema/transactions');
+
+describe('postgres transactions repository', function() {
+  let transactions
+
+  beforeEach(function() {
+    transactions = Transactions.NewPostgreSQLRepository(null, format, transactionsSchema);
+  })
+
+  it('renders the find-by-id query', async function() {
+    const result = await transactions.findByIdQuery(1);
+    expect(result).to.be.a('string');
+    expect(result).to.equal("SELECT * FROM transactions WHERE id = '1' LIMIT 1");
+  })
+
+  it('renders the save query with an emtpy account ids', async function() {
+    const transaction = transactions.new(209864836, 995070, -89.81, new Date('2019-11-11T19:31:50.379+00:00'), ' RE. 745259', 'TueV Bayern', '611105', 'DE13700800000061110500', '70080000', 'DRESDEFF700', 'Commerzbank vormals Dresdner Bank');
+
+    const result = transactions.saveQuery(transaction);
+    expect(result).to.be.a('string');
+    expect(result).to.equal("INSERT INTO transactions (id,accountid,amount,bookingdate,purpose,counterpartname,counterpartaccountnumber,counterpartiban,counterpartblz,counterpartbic,counterpartbankname) VALUES ('209864836','995070','-89.81','2019-11-11 19:31:50.379+00',' RE. 745259','TueV Bayern','611105','DE13700800000061110500','70080000','DRESDEFF700','Commerzbank vormals Dresdner Bank') ON CONFLICT (id) DO UPDATE SET (id,accountid,amount,bookingdate,purpose,counterpartname,counterpartaccountnumber,counterpartiban,counterpartblz,counterpartbic,counterpartbankname) = ('209864836','995070','-89.81','2019-11-11 19:31:50.379+00',' RE. 745259','TueV Bayern','611105','DE13700800000061110500','70080000','DRESDEFF700','Commerzbank vormals Dresdner Bank') WHERE transactions.id = '209864836'");
+  })
+
+  it('renders the delete all query', async function() {
+    const result = transactions.deleteAllQuery;
+    expect(result).to.be.a('string');
+    expect(result).to.equal("DELETE FROM transactions");
+  })
+})
