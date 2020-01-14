ac79e54a0c245d8777826676cec612529537e94f
Kiril Tonev <kiril.tonev@gmail.com>
2020-01-14 22:38:23 +0100

    Move the lambda integtests over.

---
+++ DinodimeBackend/packages/lambda/test/integration/fetch-web-form-info-handler.test.ts
@@ -0,0 +1,138 @@
+import chai from "chai";
+const expect = chai.expect;
+import winston from "winston";
+
+import { Context, APIGatewayProxyEvent } from "aws-lambda";
+import {
+  User,
+  Users,
+  BankConnections,
+  Transactions,
+  VoidTransport,
+  CallbackCrypto
+} from "dynodime-lib";
+
+import { fetchWebFormInfo } from "../../src/controllers/bank-controller";
+import { CreateFinApiTestInterfaces } from "../test-utility";
+import { EncryptedData, Encryptions } from "dynodime-lib/out/src/crypto";
+
+describe("fetch webform info handler", function() {
+  this.timeout(10000); // Webform needs time.
+
+  let logger: winston.Logger;
+  let users: Users.UsersRepository;
+  let connections: BankConnections.BankConnectionsRepository;
+  let transactions: Transactions.TransactionsRepository;
+  let context: Context;
+  let encryptions: Encryptions;
+  let encrypted: EncryptedData;
+
+  expect(process.env.FinAPIClientId).to.exist;
+  expect(process.env.FinAPIClientSecret).to.exist;
+  expect(process.env.WEBFORM_ID_FOR_FETCH).to.exist;
+  expect(process.env.ACCESS_TOKEN_FOR_FETCH).to.exist;
+
+  let dummyInterfaces = CreateFinApiTestInterfaces(
+    process.env.FinAPIClientId,
+    process.env.FinAPIClientSecret
+  );
+
+  beforeEach(function() {
+    logger = winston.createLogger({ transports: [new VoidTransport()] });
+
+    users = new Users.InMemoryRepository();
+    connections = new BankConnections.InMemoryRepository();
+    transactions = new Transactions.InMemoryRepository();
+
+    context = {} as Context;
+    encryptions = new CallbackCrypto();
+    encrypted = encryptions.EncryptText("bearer " + process.env.ACCESS_TOKEN_FOR_FETCH);
+  });
+
+  it("rejects a request with could not fetch web form", async function() {
+    const rejectedWebId = parseInt(process.env.WEBFORM_ID_FOR_FETCH + "111");
+
+    const user = new User("chapu", "chapu@mischung.net", "+666 666 666", false);
+    user.activeWebFormId = rejectedWebId;
+    user.activeWebFormAuth = encrypted.iv;
+    users.save(user);
+
+    const event = ({
+      headers: {},
+      pathParameters: {
+        webFormAuth: rejectedWebId + "-" + encrypted.cipherText
+      }
+    } as unknown) as APIGatewayProxyEvent;
+
+    const result = await fetchWebFormInfo(
+      event,
+      context,
+      logger,
+      dummyInterfaces.bankInterface,
+      users,
+      connections,
+      transactions,
+      encryptions
+    );
+
+    expect(result).to.be.an("object");
+    expect(result.statusCode).to.equal(500);
+    expect(JSON.parse(result.body).message).to.include("could not fetch web form");
+  });
+
+  it("adds a connection to the user", async function() {
+    const user = new User("chapu", "chapu@mischung.net", "+666 666 666", false);
+    user.activeWebFormId = parseInt(process.env.WEBFORM_ID_FOR_FETCH);
+    user.activeWebFormAuth = encrypted.iv;
+    users.save(user);
+
+    const event = ({
+      headers: {},
+      pathParameters: {
+        webFormAuth: process.env.WEBFORM_ID_FOR_FETCH + "-" + encrypted.cipherText
+      }
+    } as unknown) as APIGatewayProxyEvent;
+
+    const result = await fetchWebFormInfo(
+      event,
+      context,
+      logger,
+      dummyInterfaces.bankInterface,
+      users,
+      connections,
+      transactions,
+      encryptions
+    );
+    expect(result.statusCode).to.equal(200);
+    expect(result).to.be.an("object");
+
+    const responseBody = JSON.parse(result.body);
+    expect(responseBody, "the response body appears to be undefined").to.be.ok;
+    expect(responseBody).to.be.an("object", "expected the response body to be an object");
+    expect(responseBody.id).to.be.an("number", "expected the response id to be a number");
+    expect(responseBody.bankId).to.be.an("number", "expected the response bankId to be a number");
+
+    const bankConnectionId = responseBody.id;
+
+    const user_ = await users.findById("chapu");
+    expect(user_, "no user found for the given username").to.be.ok;
+    expect(user_.bankConnectionIds).to.include(
+      bankConnectionId,
+      "the connection ids were not updated"
+    );
+
+    const connection = await connections.findById(bankConnectionId);
+    expect(connection, "the connection was not created").to.be.ok;
+    expect(connection.bankAccountIds[0]).to.be.an(
+      "number",
+      "expected the bankAccountIds element to be a number"
+    );
+
+    const transactions_ = await transactions.findByAccountIds(connection.bankAccountIds);
+    expect(transactions_).to.exist;
+    expect(transactions_.length).to.not.equal(0);
+    expect(transactions_[0].id).to.exist;
+    expect(transactions_[0].accountId).to.equal(connection.bankAccountIds[0]);
+    expect(transactions_[0].absAmount).to.exist;
+  });
+});
---
+++ DinodimeBackend/packages/lambda/test/integration/get-webform-id.test.ts
@@ -0,0 +1,206 @@
+import chai from "chai";
+const expect = chai.expect;
+import winston from "winston";
+
+import { Builder, By, until } from "selenium-webdriver";
+import { Context, APIGatewayProxyEvent } from "aws-lambda";
+import {
+  User,
+  Users,
+  BankConnections,
+  FinAPI,
+  VoidTransport,
+  CallbackCrypto,
+  Encryptions
+} from "dynodime-lib";
+
+import { getWebformId } from "../../src/controllers/bank-controller";
+import { CreateFinApiTestInterfaces } from "../test-utility";
+
+describe("get webform id", function() {
+  this.timeout(10000); // Selenium browser takes so much time.
+
+  let logger: winston.Logger;
+  let users: Users.UsersRepository;
+  let context: Context;
+  let testUsername: string;
+  let testPassword: string;
+  let testValidEmail: string;
+  let testValidPhone: string;
+
+  expect(process.env.AZURE_TEST_USER_LOGIN).to.exist;
+  expect(process.env.FinAPIClientId).to.exist;
+  expect(process.env.FinAPIClientSecret).to.exist;
+
+  let dummyInterfaces = CreateFinApiTestInterfaces(
+    process.env.FinAPIClientId,
+    process.env.FinAPIClientSecret
+  );
+  let encryptions: Encryptions;
+
+  beforeEach(function() {
+    testUsername = process.env.AZURE_TEST_USER_LOGIN;
+    testPassword = process.env.AZURE_TEST_USER_LOGIN;
+    testValidEmail = "chapu@chapu.com";
+    testValidPhone = "+66 6666";
+
+    logger = winston.createLogger({ transports: [new VoidTransport()] });
+
+    users = new Users.InMemoryRepository();
+    encryptions = new CallbackCrypto();
+
+    context = {} as Context;
+  });
+
+  it("rejects requests with failing authentication", async () => {
+    users.save(new User(testUsername, testValidEmail, testValidPhone, false));
+
+    const event = ({
+      headers: {
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({ bankId: 277672 })
+    } as unknown) as APIGatewayProxyEvent;
+
+    const result = await getWebformId(
+      event,
+      context,
+      logger,
+      dummyInterfaces.bankInterface,
+      users,
+      encryptions
+    );
+
+    expect(result).to.be.an("object");
+    expect(result.statusCode).to.equal(401);
+  });
+
+  it("rejects requests because user is not available", async () => {
+    const authorization = await dummyInterfaces.authentication.getPasswordToken(
+      dummyInterfaces.clientSecrets,
+      testUsername,
+      testPassword
+    );
+
+    const event = ({
+      headers: {
+        Authorization: authorization.token_type + " " + authorization.access_token,
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({ bankId: 277672 })
+    } as unknown) as APIGatewayProxyEvent;
+
+    const result = await getWebformId(
+      event,
+      context,
+      logger,
+      dummyInterfaces.bankInterface,
+      users,
+      encryptions
+    );
+
+    expect(result).to.be.an("object");
+    expect(result.statusCode).to.equal(401);
+  });
+
+  it("rejects requests because user save failed", async () => {
+    const authorization = await dummyInterfaces.authentication.getPasswordToken(
+      dummyInterfaces.clientSecrets,
+      testUsername,
+      testPassword
+    );
+
+    const failingUsers = ({
+      findById: async (id: string) => {
+        return {
+          username: testUsername
+        };
+      },
+
+      save: async (user: User) => {
+        throw "nada";
+      }
+    } as unknown) as Users.UsersRepository;
+
+    const event = ({
+      headers: {
+        Authorization: authorization.token_type + " " + authorization.access_token,
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({ bankId: 277672 })
+    } as unknown) as APIGatewayProxyEvent;
+
+    const result = await getWebformId(
+      event,
+      context,
+      logger,
+      dummyInterfaces.bankInterface,
+      failingUsers,
+      encryptions
+    );
+
+    expect(result).to.be.an("object");
+    expect(result.statusCode).to.equal(500);
+  });
+
+  it("return webform location", async function() {
+    users.save(new User(testUsername, testValidEmail, testValidPhone, false));
+    const authorization = await dummyInterfaces.authentication.getPasswordToken(
+      dummyInterfaces.clientSecrets,
+      testUsername,
+      testPassword
+    );
+
+    const event = ({
+      headers: {
+        Authorization: authorization.token_type + " " + authorization.access_token,
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({ bankId: 277672 })
+    } as unknown) as APIGatewayProxyEvent;
+
+    const result = await getWebformId(
+      event,
+      context,
+      logger,
+      dummyInterfaces.bankInterface,
+      users,
+      encryptions
+    );
+
+    expect(result).to.be.an("object");
+    expect(result.statusCode).to.equal(200);
+    expect(JSON.parse(result.body).location).to.be.an("string");
+    expect(JSON.parse(result.body).webFormAuth).to.be.an("string");
+    console.log(JSON.parse(result.body).location);
+
+    let driver = await new Builder().forBrowser("firefox").build();
+    try {
+      await driver.get(JSON.parse(result.body).location);
+      await driver.findElement(By.id("btnSubmit")).click();
+      await driver.wait(until.elementsLocated(By.id("exitWithoutRedirect")), 1000);
+    } finally {
+      await driver.quit();
+    }
+
+    //expect(JSON.parse(result.body).location).to.equal('testlocation');
+    expect(JSON.parse(result.body).webFormAuth.split("-").length).to.equal(2);
+
+    let formId = JSON.parse(result.body).webFormAuth.split("-")[0];
+    let encryptedAuth = JSON.parse(result.body).webFormAuth.split("-")[1];
+
+    //expect(formId).to.equal('2934');
+    expect(encryptedAuth).to.not.equal(event.headers.Authorization);
+
+    // this test proves whether the right data is written to database
+    let user = await users.findByWebFormId(formId);
+    //expect(formId).to.equal(user.activeWebFormId);
+    expect(
+      encryptions.DecryptText({ iv: user.activeWebFormAuth, cipherText: encryptedAuth })
+    ).to.equal(event.headers.Authorization);
+  });
+});
