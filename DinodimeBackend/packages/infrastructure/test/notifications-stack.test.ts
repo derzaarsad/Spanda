212fc42b1928db6284bf0e05cb85d92a939931c1
Kiril Tonev <kiril.tonev@gmail.com>
2020-01-20 21:13:05 +0100

    AB#83 Fix tests.

--- DinodimeBackend/packages/infrastructure/test/notifications-stack.test.ts
+++ DinodimeBackend/packages/infrastructure/test/notifications-stack.test.ts
@@ -3,12 +3,26 @@ import mocha from "mocha";
 import cdk = require("@aws-cdk/core");
 import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
 import { Services } from "../src/services";
+import { Vpc } from "@aws-cdk/aws-ec2";
+import { LambdaDeploymentProps } from "../src/lambda-deployment-props";
 
 describe("The services stack", () => {
+  let lambdaProps: LambdaDeploymentProps;
+
+  before("setup", () => {
+    lambdaProps = {
+      vpc: {} as Vpc,
+      subnets: [],
+      securityGroups: [],
+      managedExecutionRolePolicies: []
+    };
+  });
+
   it("Contains an SQS queue", () => {
     const app = new cdk.App();
+
     // WHEN
-    const stack = new Services(app, "MyTestStack");
+    const stack = new Services(app, "MyTestStack", lambdaProps);
     // THEN
     expectCDK(stack).to(
       haveResource("AWS::SQS::Queue", {
@@ -20,7 +34,7 @@ describe("The services stack", () => {
   it("Contains an SNS topic", () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new Services(app, "MyTestStack");
+    const stack = new Services(app, "MyTestStack", lambdaProps);
     // THEN
     expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
   });
