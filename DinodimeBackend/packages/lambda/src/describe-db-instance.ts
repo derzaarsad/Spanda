01f845755d392827f952521c4fad7177c8eb9c46
Kiril Tonev <kiril.tonev@gmail.com>
2020-02-01 10:23:14 +0100

    AB#85 Iterate on the describe db instances lambda.

--- DinodimeBackend/packages/lambda/src/describe-db-instance.ts
+++ DinodimeBackend/packages/lambda/src/describe-db-instance.ts
@@ -1,5 +1,18 @@
+import winston from "winston";
 import { RDS } from "aws-sdk/clients/all";
 import { Context } from "aws-lambda";
+
+const env = process.env;
+
+const log = winston.createLogger({
+  level: env["LOGGER_LEVEL"] || "debug",
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.json()
+    })
+  ]
+});
 
 const rds = new RDS();
 
@@ -9,6 +22,7 @@ interface DatabaseInstanceQuery {
 
 interface DatabaseInstanceDetails {
   dbInstanceIdentifier: string;
+  dbInstanceArn: string;
   endpoint: string;
   port: number;
 }
@@ -17,6 +31,33 @@ export const handler = async (
   event: DatabaseInstanceQuery,
   context: Context
 ): Promise<DatabaseInstanceDetails> => {
-  rds.describeDBInstances({ DBInstanceIdentifier: event.dbInstanceIdentifier });
-  return { dbInstanceIdentifier: "test", endpoint: "test", port: 666 };
+  log.debug("Received event", event);
+
+  const dbInstanceIdentifier = event.dbInstanceIdentifier;
+
+  const description = await rds
+    .describeDBInstances({ DBInstanceIdentifier: dbInstanceIdentifier })
+    .promise()
+    .then();
+
+  const instances = description.DBInstances;
+
+  if (instances.length < 1) {
+    throw new Error(`No instances found for id ${dbInstanceIdentifier}`);
+  }
+
+  const instance = instances[0];
+
+  if (instance.DBInstanceStatus != "available") {
+    throw new Error(
+      `Expected the database to be available, but was ${instance.DBInstanceStatus} instead`
+    );
+  }
+
+  return {
+    dbInstanceIdentifier: dbInstanceIdentifier,
+    dbInstanceArn: instance.DBInstanceArn,
+    endpoint: instance.Endpoint.Address,
+    port: instance.Endpoint.Port
+  };
 };
