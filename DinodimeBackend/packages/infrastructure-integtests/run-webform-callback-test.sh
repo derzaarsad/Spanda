9eb8f57d9f97304c7545ad387cbea21f438a0ee5
Kiril Tonev <kiril.tonev@gmail.com>
2020-04-11 00:01:45 +0200

    AB#90 Fixup test case.

--- DinodimeBackend/packages/infrastructure-integtests/run-webform-callback-test.sh
+++ DinodimeBackend/packages/infrastructure-integtests/run-webform-callback-test.sh
@@ -5,13 +5,10 @@ DIRNAME=$(dirname "$0")
 
 source "$DIRNAME/common_functions.sh"
 
-apiEndpointUrl=$(getStackOutput DinodimeServices APIEndpointURL)
 callbackEndpointUrl=$(getStackOutput DinodimeServices CallbackEndpointURL)
-queueUrl=$(getStackOutput DinodimeServices WebFormCompletionsQueueURL)
-dlqUrl=$(getStackOutput DinodimeServices WebFormCompletionsDLQURL)
 adminUrl=$(getStackOutput DinodimeAdminAPI AdminEndpointURL)
 
-if [[ -z "$apiEndpointUrl" ]]; then
+if [[ -z "$callbackEndpointUrl" ]]; then
   echo "Please deploy the Dinodime API first"
   exit 1
 fi
@@ -21,9 +18,6 @@ if [[ -z "$adminUrl" ]]; then
   exit 1
 fi
 
-env API_ENDPOINT_URL=$apiEndpointUrl \
-  CALLBACK_ENDPOINT_URL=$callbackEndpointUrl \
-  QUEUE_URL=$queueUrl \
-  DLQ_URL=$dlqUrl \
+env CALLBACK_ENDPOINT_URL=$callbackEndpointUrl \
   ADMIN_ENDPOINT_URL=$adminUrl \
   yarn workspace dinodime-infrastructure-integtests run test-webform-callback $@
--- DinodimeBackend/packages/infrastructure-integtests/src/webform-callback.ts
+++ DinodimeBackend/packages/infrastructure-integtests/src/webform-callback.ts
@@ -6,16 +6,14 @@ import AWS from "aws-sdk";
 import SQS, { MessageList } from "aws-sdk/clients/sqs";
 
 import { deleteMessages } from "./util";
+import { User, Transaction, CallbackCrypto, EncryptedData } from "dinodime-lib";
 
 const assert = chai.assert;
 const expect = chai.expect;
 
 const env = process.env;
 
-const endpointUrl = env["API_ENDPOINT_URL"] as string;
 const callbackUrl = env["CALLBACK_ENDPOINT_URL"] as string;
-const queueUrl = env["QUEUE_URL"] as string;
-const dlqUrl = env["DLQ_URL"] as string;
 const adminEndpointUrl = env["ADMIN_ENDPOINT_URL"] as string;
 
 AWS.config.update({ region: process.env.CDK_DEPLOY_REGION });
@@ -23,11 +21,15 @@ AWS.config.update({ region: process.env.CDK_DEPLOY_REGION });
 describe("integration: web form callback", function () {
   this.timeout(20000);
 
+  let authenticatedSecret: EncryptedData;
   let logger: winston.Logger;
   let http: AxiosInstance;
   let sqs: SQS;
 
   before(function () {
+    const crypto = new CallbackCrypto();
+    authenticatedSecret = crypto.EncryptText("Bearer abc.def.xyz");
+
     logger = winston.createLogger({
       level: "debug",
       transports: [
@@ -40,45 +42,32 @@ describe("integration: web form callback", function () {
     sqs = new SQS();
 
     http = axios.create({
-      timeout: 3000,
+      timeout: 5000,
       headers: { Accept: "application/json" },
     });
   });
 
   it("imports the bank connections when the user is authenticated", async () => {
-    const callbackResponse = await http.get(`${callbackUrl}/webForms/callback?webFormAuth=666-covfefe`);
+    logger.info("publishing webform completion...");
+    const callbackResponse = await http.get(`${callbackUrl}/webForms/callback/666-${authenticatedSecret.cipherText}`);
     expect(callbackResponse).to.be.an("object");
     expect(callbackResponse.status).to.equal(202);
 
-    const messages: MessageList | undefined = await sqs
-      .receiveMessage({ QueueUrl: dlqUrl, AttributeNames: ["All"], WaitTimeSeconds: 5, MaxNumberOfMessages: 1 })
-      .promise()
-      .then((result) => {
-        return result.Messages;
-      });
-
-    logger.info("Received messages", messages);
-    if (!messages) {
-      logger.info(`Skipped deleting messages`);
-    } else {
-      logger.info(`Deleting ${messages.length} message(s) from queue ${dlqUrl}`);
-      await deleteMessages(sqs, queueUrl, messages);
-    }
-
-    expect(messages).to.be.ok;
-    expect(messages!.length).to.be.empty;
+    logger.info("fetching transactions...");
+    await timeout(10);
+    const transactions = await getUserTransactions();
+    expect(transactions).to.not.be.empty;
   });
 
   beforeEach(async function () {
-    const data = {
-      id: "demo",
-      password: "XXXXX",
-      email: "demo@localhost.de",
-      phone: "+49 99 999999-999",
-      isAutoUpdateEnabled: true,
-    };
-    logger.info("creating demo user");
-    await axios.post(`${endpointUrl}/users`, data);
+    const user = new User("demo", "demo@localhost.de", "+49 99 999999-999", true);
+    user.activeWebFormAuth = authenticatedSecret.iv;
+    user.activeWebFormId = 666;
+    const data = user;
+
+    logger.info("creating demo user...");
+    await axios.post(`${adminEndpointUrl}/users`, data);
+    logger.info("demo user created successfully");
   });
 
   afterEach(async function () {
@@ -87,9 +76,25 @@ describe("integration: web form callback", function () {
         Authorization: "Bearer abc.def.xyz",
       },
     };
-    logger.info("deleting bank data");
+    logger.info("deleting bank data...");
     await axios.delete(`${adminEndpointUrl}/data`, config);
+    logger.info("demo user data deleted successfully");
 
-    logger.info("deleting demo user");
+    logger.info("deleting demo user...");
+    await axios.delete(`${adminEndpointUrl}/users`, config);
+    logger.info("demo user deleted successfully");
   });
+
+  async function getUserTransactions(): Promise<Transaction[]> {
+    const config: AxiosRequestConfig = {
+      headers: {
+        Authorization: "Bearer abc.def.xyz",
+      },
+    };
+    return await axios.get(`${adminEndpointUrl}/data`, config);
+  }
+
+  function timeout(s: number) {
+    return new Promise((resolve) => setTimeout(resolve, s * 1000));
+  }
 });
