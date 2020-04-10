f46966f7ab596b7eb68287530436abf913b43e0f
Kiril Tonev <kiril.tonev@gmail.com>
2020-04-10 12:00:54 +0200

    AB#90 Get the correct stack output.

--- DinodimeBackend/packages/infrastructure-integtests/run-webform-callback-test.sh
+++ DinodimeBackend/packages/infrastructure-integtests/run-webform-callback-test.sh
@@ -5,17 +5,25 @@ DIRNAME=$(dirname "$0")
 
 source "$DIRNAME/common_functions.sh"
 
-# adminapi
-
 apiEndpointUrl=$(getStackOutput DinodimeServices APIEndpointURL)
 callbackEndpointUrl=$(getStackOutput DinodimeServices CallbackEndpointURL)
 queueUrl=$(getStackOutput DinodimeServices WebFormCompletionsQueueURL)
 dlqUrl=$(getStackOutput DinodimeServices WebFormCompletionsDLQURL)
-adminUrl=$(getStackOutput DinodimeServices AdminEndpointURL)
+adminUrl=$(getStackOutput DinodimeAdminAPI AdminEndpointURL)
+
+if [[ -z "$apiEndpointUrl" ]]; then
+  echo "Please deploy the Dinodime API first"
+  exit 1
+fi
+
+if [[ -z "$adminUrl" ]]; then
+  echo "Please deploy the Dinodime Admin API first"
+  exit 1
+fi
 
 env API_ENDPOINT_URL=$apiEndpointUrl \
   CALLBACK_ENDPOINT_URL=$callbackEndpointUrl \
   QUEUE_URL=$queueUrl \
   DLQ_URL=$dlqUrl \
-  ADMIN_URL=$adminUrl \
+  ADMIN_ENDPOINT_URL=$adminUrl \
   yarn workspace dinodime-infrastructure-integtests run test-webform-callback $@
