de440d9fa13b6f72f4c17db7b9480f03368c6383
Kiril Tonev <kiril.tonev@gmail.com>
2020-04-11 16:51:27 +0200

    AB#90  Have the test receive the decryption key.

--- DinodimeBackend/packages/infrastructure-integtests/run-webform-callback-test.sh
+++ DinodimeBackend/packages/infrastructure-integtests/run-webform-callback-test.sh
@@ -18,6 +18,12 @@ if [[ -z "$adminUrl" ]]; then
   exit 1
 fi
 
+if [[ -z "FINAPI_DECRYPTION_KEY" ]]; then
+  echo "Please indicate the finapi decryption key in the FINAPI_DECRYPTION_KEY environment variable"
+  exit 1
+fi
+
 env CALLBACK_ENDPOINT_URL=$callbackEndpointUrl \
   ADMIN_ENDPOINT_URL=$adminUrl \
+  FINAPI_DECRYPTION_KEY=$FINAPI_DECRYPTION_KEY \
   yarn workspace dinodime-infrastructure-integtests run test-webform-callback $@
--- DinodimeBackend/packages/infrastructure-integtests/src/webform-callback.ts
+++ DinodimeBackend/packages/infrastructure-integtests/src/webform-callback.ts
@@ -1,11 +1,12 @@
 import chai from "chai";
 import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 import winston from "winston";
+import qs from "querystring";
 
 import AWS from "aws-sdk";
 import SQS from "aws-sdk/clients/sqs";
 
-import { User, Transaction, CallbackCrypto, EncryptedData } from "dinodime-lib";
+import { User, Transaction, AesCrypto } from "dinodime-lib";
 
 const assert = chai.assert;
 const expect = chai.expect;
@@ -14,20 +15,21 @@ const env = process.env;
 
 const callbackUrl = env["CALLBACK_ENDPOINT_URL"] as string;
 const adminEndpointUrl = env["ADMIN_ENDPOINT_URL"] as string;
+const decryptionKey = env["FINAPI_DECRYPTION_KEY"] as string;
 
 AWS.config.update({ region: process.env.CDK_DEPLOY_REGION });
 
 describe("integration: web form callback", function () {
-  this.timeout(20000);
+  this.timeout(60000);
 
-  let authenticatedSecret: EncryptedData;
+  let authenticatedSecret: string;
   let logger: winston.Logger;
   let http: AxiosInstance;
   let sqs: SQS;
 
   before(function () {
-    const crypto = new CallbackCrypto();
-    authenticatedSecret = crypto.EncryptText("Bearer abc.def.xyz");
+    const crypto = new AesCrypto(decryptionKey);
+    authenticatedSecret = crypto.encrypt("Bearer abc.def.xyz");
 
     logger = winston.createLogger({
       level: "debug",
@@ -48,19 +50,44 @@ describe("integration: web form callback", function () {
 
   it("imports the bank connections when the user is authenticated", async () => {
     logger.info("publishing webform completion...");
-    const callbackResponse = await http.get(`${callbackUrl}/webForms/callback/666-${authenticatedSecret.cipherText}`);
+    const completion = qs.escape(`666-${authenticatedSecret}`);
+    const callbackResponse = await http.get(`${callbackUrl}/webForms/callback/${completion}`).catch((err) => {
+      assert.fail(`The callback function returned an error: ${err}`);
+      return err;
+    });
     expect(callbackResponse).to.be.an("object");
     expect(callbackResponse.status).to.equal(202);
 
     logger.info("fetching transactions...");
     await timeout(10);
-    const transactions = await getUserTransactions();
+    const transactions = await getUserTransactions().catch((err) => {
+      assert.fail(`Could not obtain transactions from the adimn api: ${err}`);
+      return err;
+    });
     expect(transactions).to.not.be.empty;
+  });
+
+  it("reports success on unauthenticated user", async () => {
+    logger.info("publishing webform completion...");
+    const completion = qs.escape("1-covfefe");
+    const callbackResponse = await http.get(`${callbackUrl}/webForms/callback/${completion}}`).catch((err) => {
+      assert.fail(`The callback function returned an error: ${err}`);
+      return err;
+    });
+    expect(callbackResponse).to.be.an("object");
+    expect(callbackResponse.status).to.equal(202);
+
+    logger.info("fetching transactions...");
+    await timeout(10);
+    const transactions = await getUserTransactions().catch((err) => {
+      assert.fail(`Could not obtain transactions from the adimn api: ${err}`);
+    });
+    expect(transactions).to.be.empty;
   });
 
   beforeEach(async function () {
     const user = new User("demo", "demo@localhost.de", "+49 99 999999-999", true);
-    user.activeWebFormAuth = authenticatedSecret.iv;
+    user.activeWebFormAuth = authenticatedSecret;
     user.activeWebFormId = 666;
     const data = user;
 
@@ -90,7 +117,7 @@ describe("integration: web form callback", function () {
         Authorization: "Bearer abc.def.xyz",
       },
     };
-    return await axios.get(`${adminEndpointUrl}/data`, config);
+    return await axios.get(`${adminEndpointUrl}/data`, config).then((response) => response.data);
   }
 
   function timeout(s: number) {
