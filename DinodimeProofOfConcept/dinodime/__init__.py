c3a80f1de9b8755da24a43b7c6902a3b0aa28e81
Derza Arsad <derza.arsad@infactory-solutions.net>
2021-03-17 18:37:42 +0100

    adding income class

--- DinodimeProofOfConcept/dinodime/__init__.py
+++ DinodimeProofOfConcept/dinodime/__init__.py
@@ -12,6 +12,46 @@ class CashActivity:
     def __str__(self):
         return str(self.cash_amount) + " " + str(self.work_effort) + " " + str(self.execution_date.strftime("%Y-%m-%d"))
 
+class Income:
+    def __init__(self, return_days, work_effort):
+        assert isinstance(return_days,int)
+        assert isinstance(work_effort, timedelta)
+        if return_days < 0:
+            raise ValueError("The profit cannot be received in the past!")
+        self.return_days = return_days
+        self.work_effort = work_effort
+
+    def __createDividendActivity__(self,start_date,return_date):
+        raise ValueError("This method must be overriden!")
+
+    def __profitModel__(self,investment):
+        raise ValueError("This method must be overriden!")
+
+    def createCashActivities(self,investment,start_date):
+        return_date = start_date + timedelta(days=self.return_days)
+        return self.createDividendActivity(start_date,return_date) + [CashActivity(self.profitModel(investment),self.work_effort,return_date)]
+
+class SellGoods(Income):
+    def __init__(self,sale_price):
+        Income.__init__(self, 0, timedelta(0,0,0,0,0,0,0))
+        self.sale_price = sale_price
+
+    def __profitModel__(self,investment):
+        raise self.sale_price
+
+    def __createDividendActivity__(self,start_date,return_date):
+        return []
+
+class StockInvestment(Income):
+    def __init__(self, return_days):
+        Income.__init__(self, return_days, timedelta(0,0,0,0,0,0,0))
+
+    def __profitModel__(self,investment):
+        raise ValueError("This method must be overriden!")
+
+    def __createDividendActivity__(self,start_date,return_date):
+        raise ValueError("This method must be overriden!")
+
 # Includes date1 and date2
 def daterange(date1, date2):
     assert isinstance(date1, date)
